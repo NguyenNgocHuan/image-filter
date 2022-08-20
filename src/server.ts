c044222d4aa4dea8c145eb8c419710272f19d5af
Ngọc Huấn Nguyễn <ngochuannguyen96@gmail.com>
2022-08-20 10:32:54 +0700

    validate url and update filter function

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 
 import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {filterImageFromURL, deleteLocalFiles, isUrlValid} from './util/util';
+import fetch from 'cross-fetch';
 (async () => {
   // Init the Express application
   const app = express();
@@ -12,53 +13,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
   app.get('/filteredimage', async (req: Request, res: Response) => {
     let {image_url} = req.query;
-
-    // const response = await fetch('https://google.com', {
-    //     method: 'HEAD'
-    // });
-    
-    // console.log(response.ok);
-    // console.log(image_url);
-    // if (!image_url) {
-    //   return res.status(400).send(`image_url is required`);
-    // }
-    // try {
-    //   const response = await fetch(image_url);
-    //   if (!response.ok) {
-    //     throw new Error(`Error! status: ${response.status}`);
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    // }
-    // fetch(image_url).then((data) => {
-    //   console.log(data);
-    // });
-    filterImageFromURL(image_url).then((data) => {
-      if (data) {
-        deleteLocalFiles(image_url);
-        return res.status(200).send(data);
+    if (!isUrlValid(image_url)) {
+      return res.status(400).send(`image_url is invalid`);
+    }
+    fetch(image_url).then(data => {
+      if (data.ok) {
+        filterImageFromURL(image_url).then((data) => {
+        if (data) {
+          deleteLocalFiles([data]);
+          return res.status(200).send(data);
+        }
+        return res.status(404).send(`image_url not found`);
+        });
+      } else {
+        return res.status(404).send(`image_url not found`);
       }
-      return res.status(404).send(`image_url not found`);
-    });
-    return res.status(404).send(`image_url not found`);
+    })
   });
   //! END @TODO1
   
--- src/util/util.ts
+++ src/util/util.ts
@@ -13,7 +13,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
     try {
       const photo = await Jimp.read(inputURL);
       const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+        "\\tmp\\filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
@@ -36,4 +36,12 @@ export async function deleteLocalFiles(files: Array<string>) {
   for (let file of files) {
     fs.unlinkSync(file);
   }
+}
+// validate url
+export function isUrlValid(inputUrl: string) {
+  var res = inputUrl.match(/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/);
+  if(res == null)
+      return false;
+  else
+      return true;
 }
